2d4d0cb5c8383136badb1022d82ee1c3b7d12f9f
Matthew Rust <rust8@purdue.edu>
2022-09-20 14:18:10 -0400

    moved cypher conversion to its own function

--- CGT 215 Lab05/CGT 215 Lab05.cpp
+++ CGT 215 Lab05/CGT 215 Lab05.cpp
@@ -9,34 +9,44 @@
 
 using namespace std;
 
-
-
-int main()
+void cypherConversion(string text)
 {
-	string text;
 	int lineLength;
+	int cypherLocation;
+	char toUpperCase;
+	vector<char> cypher = { 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
 
-	cout << "enter a line: ";
-	getline(cin, text);
-
-	lineLength = size(text);
-	cout << lineLength << endl;
+	lineLength = size(text); 
 
 	for (int j = 0; j < lineLength; j++)
 	{
 		if (text[j] >= 65 && text[j] <= 90) //checks if letter is upper case
 		{
-			cout << 'U' << endl;
+			cypherLocation = text[j] - 65; // lowers number to match with the cypher vector
+			cout << cypher[cypherLocation]; // converts letter based on the cypher
 		}
 		else if (text[j] >= 97 && text[j] <= 122) // checks if letter is lower case
 		{
-			cout << "L" << endl;
+			toUpperCase = text[j] - 32; // sets lower case to upper case
+			cypherLocation = toUpperCase - 65;//matches to cypher
+			toUpperCase = cypher[cypherLocation] + 32; // back to lower case
+			cout << toUpperCase; // displays new letter
 		}
 		else // if it is a number or special character
 		{
-			cout << "S" << endl;
+			cout << text[j];
 		}
 	}
+}
+
+int main()
+{
+	string sentence;
+
+	cout << "enter a line: ";
+	getline(cin, sentence);
+
+	cypherConversion(sentence);
 }
 
 
