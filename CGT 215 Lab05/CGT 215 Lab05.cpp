c8f383495279a3a3da4179b3f4162339c0ab1fb7
Matthew Rust <rust8@purdue.edu>
2022-09-20 13:36:31 -0400

    Add project files.

---
+++ CGT 215 Lab05/CGT 215 Lab05.cpp
@@ -0,0 +1,19 @@
+// CGT 215 Lab05.cpp : 
+// This file contains the 'main' function. 
+// Program execution begins and ends there.
+// Written by Matthew Rust 9/20/2022
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	string text;
+	cout << "enter a line: ";
+	getline(cin, text);
+}
+
+
