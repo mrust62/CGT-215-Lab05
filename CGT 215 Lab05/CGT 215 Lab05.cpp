c0d37da684019d013a073ae4fdbf2f0c44364327
Matthew Rust <rust8@purdue.edu>
2022-09-20 13:55:13 -0400

    now reads the sentence and determine if letters are upper or lower case

--- CGT 215 Lab05/CGT 215 Lab05.cpp
+++ CGT 215 Lab05/CGT 215 Lab05.cpp
@@ -9,11 +9,34 @@
 
 using namespace std;
 
+
+
 int main()
 {
 	string text;
+	int lineLength;
+
 	cout << "enter a line: ";
 	getline(cin, text);
+
+	lineLength = size(text);
+	cout << lineLength << endl;
+
+	for (int j = 0; j < lineLength; j++)
+	{
+		if (text[j] >= 65 && text[j] <= 90) //checks if letter is upper case
+		{
+			cout << 'U' << endl;
+		}
+		else if (text[j] >= 97 && text[j] <= 122) // checks if letter is lower case
+		{
+			cout << "L" << endl;
+		}
+		else // if it is a number or special character
+		{
+			cout << "S" << endl;
+		}
+	}
 }
 
 
